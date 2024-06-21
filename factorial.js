c0e6690b1069283942704f8529c45554b9629deb
rpaternina <robertpaternina65@gmail.com>
2024-06-20 19:17:43 -0500

    Practicando un poco más, usando for y if

---
+++ factorial.js
@@ -0,0 +1,14 @@
+// Escribe un programa que calcule el 
+// factorial de un número dado por el usuario.
+
+
+let contador = 1;
+let num = prompt('Ingrese un numero: ');
+for(let i = 1; i <= num; i *= i){
+    if(num >= 1){
+        // multi = factorial * contador;
+        document.write(i + ", ");
+
+    }
+        
+}
