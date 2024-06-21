f7dd1043c71130131031bd3668cde0c1cc5ed398
rpaternina <robertpaternina65@gmail.com>
2024-06-21 17:38:54 -0500

    Desde aqui ya uso la consola con nodejs para ejecutar los archivos

---
+++ fibonaci.js
@@ -0,0 +1,12 @@
+
+function par(num){
+    if(num % 2 === 0){
+        console.log(`El numero ${num} es par`);
+    }else{
+        console.log(`El numero ${num} es impar`);
+    }
+}
+
+par(2);
+par(4);
+par(9);
