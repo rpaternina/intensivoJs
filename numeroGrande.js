7aaafe4c9ddb2f4e3b433cdb53f05a4a6995d41e
rpaternina <robertpaternina65@gmail.com>
2024-06-23 18:13:01 -0500

    Listo, corregi todo

--- numeroGrande.js
+++ numeroGrande.js
@@ -1,11 +1,17 @@
 // Escribe un programa que encuentre y devuelva el número más grande en un array de números.
 
-let numeros = [70,1,2,3,4,5,10,1,2,3,7,50,1,900,2];
+let numeros = [70,1,2,3,4,5,10,1,2,3,7,50,1,900,2,1000];
+
+//creo una nueva variable y le asigno la posicion [0] de mi array numeros
+let numeroGrande = numeros[0];
 
 for(let i = 0; i < numeros.length; i++){
-    //comparo el numero anterior con el siguiente, y si el anterior es mayor se queda con ese
-    if(numeros[i-1] > numeros[i]){
-        console.log(`El numero mayor es: ${numeros[i-1]}`);
+    //comparo si el numero actualo es mayor al anterior, si no es así que siga la secuencia
+    if(numeros[i] > numeroGrande){
+        //una vez encuentre el numero mayor se le asigana a numero grande esa posicion
+        numeroGrande = numeros[i];
+        
     }
     
 }
+console.log(`El numero mayor es: ${numeroGrande}`);
