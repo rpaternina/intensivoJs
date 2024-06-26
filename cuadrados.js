4aeac404154be0a282fc6d5eee95303c5b94a0b9
rpaternina <robertpaternina65@gmail.com>
2024-06-26 15:27:50 -0500

    .

---
+++ cuadrados.js
@@ -0,0 +1,13 @@
+//Escribe un programa que recorra un array de números y devuelva un nuevo array con los cuadrados de cada número.
+
+let cuadrados = [1,2,3,4,5];
+
+let multi = 0;
+
+for(let i = 0; i < cuadrados.length; i++){
+
+    multi = cuadrados[i]**2
+    console.log(multi);
+    
+}
+
