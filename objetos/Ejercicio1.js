3c5d9bc714784e39d1b2db8a384bcdd3eb83bc94
rpaternina <robertpaternina65@gmail.com>
2024-07-01 17:11:20 -0500

    Aqui estan todos los ejercicios relacionados con objetos

---
+++ objetos/Ejercicio1.js
@@ -0,0 +1,10 @@
+// Crea un objeto que represente un libro, con propiedades como título, autor y año de publicación. 
+// Accede a estas propiedades e imprímelas en la consola.
+
+let libro = {
+    titulo: 'La divina comedia',
+    autor: 'Dante',
+    año: '1700'
+}
+
+console.log(`${libro.titulo} , ${libro.autor} , ${libro.año}`) ;
