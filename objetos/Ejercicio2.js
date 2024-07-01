e01a6719ffe70d85aa9b742d32539d94d3e6e4a8
rpaternina <robertpaternina65@gmail.com>
2024-07-01 17:19:38 -0500

    Añadiendo y modificando propiedades

---
+++ objetos/Ejercicio2.js
@@ -0,0 +1,13 @@
+// Crea un objeto que represente un coche con propiedades como marca, modelo y año. 
+// Luego, añade una nueva propiedad llamada color y modifica el valor de año.
+
+let coche = {
+    marca: 'Mazda',
+    modelo: 'H1',
+    año: '2021'
+}
+
+coche.color = 'Rojo';
+coche.año = '2022';
+
+console.log(coche);
